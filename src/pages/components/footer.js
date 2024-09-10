4c362f5a53d442e15860f1051f7a8158310f29bf
Camilla Soares Sousa <camillasoares818@gmail.com>
2024-09-10 12:09:46 -0300

    Footer

--- src/pages/components/footer.js
+++ src/pages/components/footer.js
@@ -1,16 +1,46 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from '../../styles/Footer.module.css';
+
 const Footer = () => {
-    return (
-      <footer className="footer">
-        <p>Encontre seu próximo filme favorito com o Cinematch: onde a paixão pelo cinema encontra a tecnologia.</p>
-        <ul className="footer-links">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Descubra</a></li>
-          <li><a href="#">Sobre</a></li>
-          <li><a href="#">Perfil</a></li>
-        </ul>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.leftSection}>
+          <p className={styles.text}>
+            Encontre seu próximo filme favorito com o Cinematch: onde a paixão pelo cinema encontra a tecnologia.
+          </p>
+          <div className={styles.links}>
+            <Link href="/politica-privacidade">Política de Privacidade</Link>
+            <span> | </span>
+            <Link href="/termos-servico">Termos de Serviço</Link>
+            <span> | </span>
+            <Link href="/linguagem">Linguagem</Link>
+          </div>
+        </div>
+        <div className={styles.rightSection}>
+          <div className={styles.navigation}>
+            <Link href="/">Home</Link>
+            <span> / </span>
+            <Link href="/descubra">Descubra</Link>
+            <span> / </span>
+            <Link href="/sobre">Sobre</Link>
+            <span> / </span>
+            <Link href="/perfil">Perfil</Link>
+          </div>
+          <div className={styles.socialIcons}>
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              <Image src="/assets/github.svg" alt="github" width={24} height={24} className={styles.icon} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.copyright}>
+        <p>&copy; 2024</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/timeline.js
+++ src/pages/timeline.js
@@ -2,8 +2,6 @@ import Carrossel from './components/carrossel';
 import Footer from './components/footer';
 import Navbar from './components/navbar';
 import Platforms from './components/platforms';
-import Section from './components/section';
-import { movies, releases, series } from './data/itens';
 import styles from '../styles/Timeline.module.css';
 
 
@@ -14,9 +12,7 @@ const Timeline = () => {
                 <Navbar />
                 <Carrossel />
                 <Platforms />
-                <Section title="Lançamentos" items={releases} />
-                <Section title="Séries que talvez você goste" items={series} />
-                <Section title="Filmes que talvez você goste" items={movies} />
+
                 <Footer />
             </div>
         </>
