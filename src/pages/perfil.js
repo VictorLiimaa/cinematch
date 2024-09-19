0a5958b54b7a328fdf57b0f237b8a51b1b4bd98b
Livya kelly <livyakellysilva.lk@gmail.com>
2024-09-19 18:06:44 -0300

    a

--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -1,11 +1,6 @@
 import InfosPerfil from "../pages/components/infosPerfil";
-import Dropdowns from "../pages/components/dropdown";
+
 
 export default function Perfil() {
-    return (
-        <>
-            <InfosPerfil />
-            <Dropdowns />
-        </>
-    );
+    return <InfosPerfil/>;
 }
