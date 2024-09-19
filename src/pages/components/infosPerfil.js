4b11d1ceca145311adff453092ed501accc7d1d6
Livya kelly <livyakellysilva.lk@gmail.com>
2024-09-19 17:53:40 -0300

    correçao

--- src/pages/components/infosPerfil.js
+++ src/pages/components/infosPerfil.js
@@ -4,7 +4,6 @@ import Cadeado from "../../../public/assets/cadeado.svg";
 import Email from "../../../public/assets/email.svg";
 import EditarFoto from "../../../public/assets/editar foto.svg";
 import styles from "../../styles/InfosPerfil.module.css";
-import Dropdowns from "./dropdown";
 
 export default function InfosPerfil() {
   return (
@@ -53,7 +52,6 @@ export default function InfosPerfil() {
       {/* Container das listas */}
       <div className={styles.containerListas}>
         <div className={styles.listas}>
-          <Dropdowns />
         </div>
       </div>
     </div>
--- src/pages/perfil.js
+++ src/pages/perfil.js
@@ -1,5 +1,11 @@
 import InfosPerfil from "../pages/components/infosPerfil";
+import Dropdowns from "../pages/components/dropdown";
 
 export default function Perfil() {
-    return <InfosPerfil/>;
+    return (
+        <>
+            <InfosPerfil />
+            <Dropdowns />
+        </>
+    );
 }
